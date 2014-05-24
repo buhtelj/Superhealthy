3488188d451585e96cc20d8cf45a59e53eb094dc
buhtelj <grcar.urban@gmail.com>
2014-05-24 11:34:34 +0200

    #3

--- www/js/funkcije.js
+++ www/js/funkcije.js
@@ -27,3 +27,16 @@
 	function exitFromApp(){
 		navigator.app.exitApp();
 	}
+	
+	
+	function getQueryVariable(variable) {
+		var query = window.location.search.substring(1);
+		var vars = query.split("&amp;");
+		for (var i=0; i<vars.length;i++) {
+		  var pair = vars[i].split("=");
+		  if (pair[0] == variable) {
+		  return pair[1];
+		}
+	  }
+	  alert('Query Variable ' + variable + ' not found');
+	}
